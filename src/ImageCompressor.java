ecc10629619e4c51d83800d55bef2047f042e28f
BenedictusNelson <benedictus.nelson@gmail.com>
2025-04-11 15:50:11 +0700

    feat: implementasi fondasi program + Variance errorMethod
    - Implementasi ImageCompressor
    - Penghitungan error masih Variance.
    - Hapus CustomImageCompressor karena tidak terpakai.

--- src/ImageCompressor.java
+++ src/ImageCompressor.java
@@ -1,29 +1,27 @@
-// import java.awt.image.BufferedImage;
-// import java.awt.Color;
-// import java.io.File;
-// import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.awt.Color;
+import java.io.File;
+import javax.imageio.ImageIO;
 
-// public class ImageCompressor {
-//     // Parameter input
-//     private String inputPath;
-//     private int errorMethod;
-//     private double threshold;
-//     private int minBlockSize;
-//     private double targetCompression;
-//     private String outputImagePath;
+public class ImageCompressor {
+    // Parameter input
+    private String inputPath;
+    private double threshold;
+    private int minBlockSize;
+    private String outputImagePath;
+
+    // Variabel untuk nyimpan gambar
+    private BufferedImage originalImage;
+    private BufferedImage compressedImage;
+    private QuadTreeNode root;
     
-//     // Variabel untuk nyimpan gambar
-//     private BufferedImage originalImage;
-//     private BufferedImage compressedImage;
-//     private QuadTreeNode root;
-    
-//     // Statistik output
-//     private long executionTime;
-//     private long originalSize;
-//     private long compressedSize;
-//     private int treeDepth;
-//     private int totalNodes;
-//     private double compressionPercentage;
+    // Statistik output
+    private long executionTime;
+    private long originalSize;
+    private long compressedSize;
+    private int treeDepth;
+    private int totalNodes;
+    private double compressionPercentage;
     
     /**
      * Konstruktor ImageCompressor.
@@ -32,10 +30,8 @@
      */
     public ImageCompressor(String inputPath, int errorMethod, double threshold, int minBlockSize, double targetCompression, String outputImagePath) {
         this.inputPath = inputPath;
-        this.errorMethod = errorMethod;
         this.threshold = threshold;
         this.minBlockSize = minBlockSize;
-        this.targetCompression = targetCompression;
         this.outputImagePath = outputImagePath;
     }
     
@@ -45,7 +41,29 @@
      * F.S.: Gambar hasil kompresi disimpan beserta statistik output
      */
     public void compress() {
-      
+        try {
+            // baca gambar dan ukuran
+            originalImage = ImageIO.read(new File(inputPath));
+            originalSize = new File(inputPath).length();
+            
+            long startTime = System.currentTimeMillis();
+            // bangun quadtree
+            root = buildQuadTree(0, 0, originalImage.getWidth(), originalImage.getHeight());
+            
+            // image reconstruction from quadtree
+            compressedImage = reconstructImage(root, originalImage.getWidth(), originalImage.getHeight());
+            ImageIO.write(compressedImage, "png", new File(outputImagePath));
+            compressedSize = new File(outputImagePath).length();
+            
+            // hitung stats kompresi
+            compressionPercentage = (1 - ((double)compressedSize / originalSize)) * 100;
+            executionTime = System.currentTimeMillis() - startTime;
+            treeDepth = calculateTreeDepth(root);
+            totalNodes = countNodes(root);
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     
     /**
@@ -54,9 +72,30 @@
      * F.S.: Return node Quadtree yang merepresentasikan blok gambar
      */
     private QuadTreeNode buildQuadTree(int x, int y, int width, int height) {
-       
+        double error = calculateError(x, y, width, height);
+        
+        // cek kondisi penghentian:
+        if (error <= threshold || width <= minBlockSize || height <= minBlockSize) {
+            Color avgColor = calculateAverageColor(x, y, width, height);
+            return new QuadTreeNode(x, y, width, height, avgColor);
+        } else {
+            int halfWidth = width / 2;
+            int halfHeight = height / 2;
+            
+            if (halfWidth == 0 || halfHeight == 0) {
+                Color avgColor = calculateAverageColor(x, y, width, height);
+                return new QuadTreeNode(x, y, width, height, avgColor);
+            }
+            
+            QuadTreeNode child1 = buildQuadTree(x, y, halfWidth, halfHeight);
+            QuadTreeNode child2 = buildQuadTree(x + halfWidth, y, width - halfWidth, halfHeight);
+            QuadTreeNode child3 = buildQuadTree(x, y + halfHeight, halfWidth, height - halfHeight);
+            QuadTreeNode child4 = buildQuadTree(x + halfWidth, y + halfHeight, width - halfWidth, height - halfHeight);
+            
+            QuadTreeNode[] children = new QuadTreeNode[]{child1, child2, child3, child4};
             return new QuadTreeNode(x, y, width, height, children);
         }
+    }
     
     /**
      * Menghitung error pada blok gambar berdasarkan metode yang dipilih.
@@ -64,7 +103,19 @@
      * F.S.: Mengembalikan nilai error 
      */
     private double calculateError(int x, int y, int width, int height) {
+        double sum = 0, sumSq = 0;
+        int count = width * height;
+        
+        for (int i = x; i < x + width; i++) {
+            for (int j = y; j < y + height; j++) {
+                Color c = new Color(originalImage.getRGB(i, j));
 
+                int pixelValue = (c.getRed() + c.getGreen() + c.getBlue()) / 3;
+                sum += pixelValue;
+                sumSq += pixelValue * pixelValue;
+            }
+        }
+        double mean = sum / count;
         return (sumSq / count) - (mean * mean);
     }
     
@@ -74,7 +125,21 @@
      * F.S.: Mengembalikan nilai Color rata-rata untuk blok
      */
     private Color calculateAverageColor(int x, int y, int width, int height) {
-       
+        long sumR = 0, sumG = 0, sumB = 0;
+        int count = width * height;
+        
+        for (int i = x; i < x + width; i++) {
+            for (int j = y; j < y + height; j++) {
+                Color c = new Color(originalImage.getRGB(i, j));
+                sumR += c.getRed();
+                sumG += c.getGreen();
+                sumB += c.getBlue();
+            }
+        }
+        
+        int avgR = (int)(sumR / count);
+        int avgG = (int)(sumG / count);
+        int avgB = (int)(sumB / count);
         return new Color(avgR, avgG, avgB);
     }
     
@@ -84,13 +149,24 @@
      * F.S.: Mengembalikan BufferedImage hasil rekonstruksi
      */
     private BufferedImage reconstructImage(QuadTreeNode node, int width, int height) {
-       
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        reconstructHelper(node, image);
         return image;
     }
     
     // Fungsi rekursif untuk mewarnai gambar 
     private void reconstructHelper(QuadTreeNode node, BufferedImage image) {
-       
+        if (node.isLeaf()) {
+            for (int i = node.x; i < node.x + node.width; i++) {
+                for (int j = node.y; j < node.y + node.height; j++) {
+                    image.setRGB(i, j, node.averageColor.getRGB());
+                }
+            }
+        } else {
+            for (QuadTreeNode child : node.children) {
+                reconstructHelper(child, image);
+            }
+        }
     }
     
     /**
@@ -99,10 +175,19 @@
      * F.S.: Return maxDepth pohon
      */
     private int calculateTreeDepth(QuadTreeNode node) {
-      
+        if (node.isLeaf()) {
+            return 1;
+        } else {
+            int maxDepth = 0;
+            for (QuadTreeNode child : node.children) {
+                int childDepth = calculateTreeDepth(child);
+                if (childDepth > maxDepth) {
+                    maxDepth = childDepth;
+                }
+            }
             return maxDepth + 1;
         }
-    
+    }
     
     /**
      * Menghitung jumlah total simpul pada Quadtree.
@@ -110,10 +195,16 @@
      * F.S.: Return jumlah node pada pohon
      */
     private int countNodes(QuadTreeNode node) {
-     
+        if (node.isLeaf()) {
+            return 1;
+        } else {
+            int count = 1;
+            for (QuadTreeNode child : node.children) {
+                count += countNodes(child);
+            }
             return count;
         }
-    
+    }
     
     // Getter untuk output stats
     public long getExecutionTime() {
@@ -139,4 +230,4 @@
     public int getTotalNodes() {
         return totalNodes;
     }
-
+}
